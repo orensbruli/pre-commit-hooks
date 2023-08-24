c6fc59e4f98b75c3650a443ee3f3dfb54322d882
orensbruli <orensbruli@gmail.com>
2023-08-24 10:14:00 +0200

    Working version

--- pre_commit_hooks/check_lfs_patterns.py
+++ pre_commit_hooks/check_lfs_patterns.py
@@ -15,52 +15,53 @@ def get_lfs_patterns() -> List[str]:
     return re.findall(r'^\s*(\*\.[A-Za-z0-9]+)', output, re.MULTILINE)
 
 
-def check_file_in_lfs(filename: str) -> bool:
-    try:
-        output = subprocess.check_output(
-            ["git", "lfs", "ls-files"],
-            encoding='utf-8'
-        ).strip()
-        return filename in output
-    except subprocess.CalledProcessError:
-        return False
+def files_in_lfs() -> str:
+    return cmd_output("git", "lfs", "ls-files").strip()
+
+def pattern_list_to_regex(patterns: List[str]) -> str:
+    # Convertir los patrones fnmatch a una expresión regular
+    regex = '|'.join([re.escape(pattern).replace('\\*', '.*') for pattern in patterns])
+    return re.compile(regex)
 
 
 def check_files_against_lfs_patterns(
         filenames: Sequence[str],
-        *,
         enforce_all: bool = False
 ) -> int:
     retv = 0
     lfs_patterns = get_lfs_patterns()
     if enforce_all:
-        print("Enforcing all files are checked.")
+        # print("Enforcing all files are checked.")
         filenames = set(cmd_output('git', 'ls-files').splitlines())
-    print(f"enforce_all: {enforce_all}")
-    print(f"Checking {len(lfs_patterns)} LFS patterns against {len(filenames)} files.")
-    print(f"Patterns: {lfs_patterns}")
+    patterns_regex = pattern_list_to_regex(lfs_patterns)
+    tracked_lfs_files = files_in_lfs()
+    non_tracked_lfs_files = []
     for filename in filenames:
-        for pattern in lfs_patterns:
-            if fnmatch(pattern, filename) and not check_file_in_lfs(filename):
-                print(f"File '{filename}' matches pattern '{pattern}' but is not tracked by LFS.")
+        if patterns_regex.match(filename):
+            if filename not in tracked_lfs_files:
+                non_tracked_lfs_files.append(filename)
                 retv = 1
+    if retv:
+        print(" ".join(lfs_patterns))
+        print("Files matching these patterns must be tracked by LFS.")
+        print("\t"+"\n\t".join(non_tracked_lfs_files))
+        print("If these files already exist in the repository, you can convert them to LFS with:")
+        print(f"  git lfs migrate import --include=\"{' '.join(non_tracked_lfs_files)}\" --no-rewrite")
+        print("If these files are new, you should check that you have lfs configured correctly:")
+        print("  git lfs install")
+        print("If it's installed you can track files with:")
+        print(f"  git reset {' '.join(non_tracked_lfs_files)}; git lfs track {' '.join(non_tracked_lfs_files)}")
+        print("You can check which files are tracked by LFS with:")
+        print("  git lfs ls-files")
     return retv
 
 
 def main(argv: Sequence[str] | None = None) -> int:
-    print(f"Running {__file__} with argv: {argv}")
+    # print(f"Running {__file__} with argv: {argv}")
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'filenames', nargs='*',
-        help='Filenames pre-commit believes are changed.',
-    )
-    parser.add_argument(
-        '--all-files', action='store_true',
-        help='Enforce all files are checked, not just staged files.',
-    )
-
+    parser.add_argument('filenames', nargs='*', help='Filenames pre-commit believes are changed.')
+    parser.add_argument('--all-files', action='store_true', help='Check all files in the repository.')
     args = parser.parse_args(argv)
-
     return check_files_against_lfs_patterns(
         args.filenames,
         enforce_all=args.all_files
